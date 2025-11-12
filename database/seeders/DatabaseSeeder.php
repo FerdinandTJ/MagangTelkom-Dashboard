5427000a83f47bfcc52a91c73b513307a7e31db4
Ferdinand Tjondrojo <ferdinandtj4@gmail.com>
2025-11-12 16:49:48 +0700

    merge databaseeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,10 @@ class DatabaseSeeder extends Seeder
             ]
         );
 
+        // Seed companies and revenue data
+        // $this->call([
+        //     CompanyRevenueSeeder::class,
+        // ]);
         // Seed master data in order of dependencies
         // Note: Regions are auto-seeded in migration 2025_11_12_000004
         $this->call([
